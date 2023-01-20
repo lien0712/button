bd4dde7ce32edb7c56082888b6ab5ee4a22a9088
lien0712 <vuthilien712@gmail.com>
2023-01-20 09:02:33 +0700

    1

--- app/src/main/java/com/example/buttonradio/MainActivity.java
+++ app/src/main/java/com/example/buttonradio/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.buttonradio;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
@@ -10,6 +16,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner label_spinner;
+    String cake ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +24,24 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         label_spinner=findViewById(R.id.label_spinner);
         label_spinner.setOnItemSelectedListener(this);
 
+    }
+    public void onclick(View view) {
+        Intent intent= new Intent(this,activityOrder.class);
+        intent.putExtra("cake",cake);
+        startActivity(intent);
+
+    }
+    public void orderCake(View view) {
+        if (view.getId() == R.id.imagedonut) {
+            cake="donut";
+            Toast.makeText(this, R.string.donut_order, Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.imageicecream) {
+            cake="icecream";
+            Toast.makeText(this, R.string.ice_cream_order, Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.imagecake) {
+            cake="cake";
+            Toast.makeText(this, R.string.cake_order, Toast.LENGTH_SHORT).show();
+        }
     }
     public void onRadioButtonClicked(View view) {
         switch (view.getId()){
@@ -46,5 +71,39 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void order(MenuItem item) {
+        displayToast("Order");
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_order:
+                AlertDialog.Builder myalert= new AlertDialog.Builder(MainActivity.this);
+                myalert.setTitle("Thnah toan");
+                myalert.show();
+                myalert.setMessage("Sure?");
+                myalert.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(this,activityOrder.class));
+                    }
+                });
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+            case R.id.action_setting:
+                displayToast(getString(R.string.action_status_message));
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/buttonradio/activityOrder.java
+++ app/src/main/java/com/example/buttonradio/activityOrder.java
@@ -1,18 +1,29 @@
 package com.example.buttonradio;
 
+import static com.example.buttonradio.R.id.tvCake;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class activityOrder extends AppCompatActivity {
-
+    TextView tvCake;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        tvCake= findViewById(R.id.tvCake);
+
+        Intent intent= getIntent();
+        if(intent!=null){
+            String cake=intent.getStringExtra("cake");
+            tvCake.setText(cake);
+        }
     }
 
 
